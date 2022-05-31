c51810f7781ffda88066b6377137b344b30e4276
ozasweita <60314240+ozasweita@users.noreply.github.com>
2022-05-31 13:12:52 +0545

    added new files

---
+++ kukaru/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Ankush"&&b=="kukukuki")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="surprise.html";
+  	}
+  	
